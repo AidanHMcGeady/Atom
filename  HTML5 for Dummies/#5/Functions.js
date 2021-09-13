6106a664cf6cd3459516a5344e30088ff215a3ae
Aidan McGeady <74266562+AidanHMcGeady@users.noreply.github.com>
2021-09-13 22:33:11 +0100

    13/09 #3

---  HTML5 for Dummies/#5/Functions.js
+++  HTML5 for Dummies/#5/Functions.js
@@ -1,7 +1,9 @@
 var output;
+var i;
 
 function init() {
   output = document.getElementBtId("output");
+  i = document.getElementBtId("i");
 }
 
 function count() {
@@ -16,12 +18,12 @@ function backwards() {
   output.innerHTML = "";
 
   for (i; i >= 0; i--) {
-    output.innerHTML -= i + "<br />";
+    output.innerHTML += i + "<br />";
   }
 }
 
 function byFive() {
-  i = i + 5;
+  i += 5;
 
   output.innerHTML = i + "<br />";
 }
